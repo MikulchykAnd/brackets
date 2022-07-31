6d4d3001a2d2cf85dbff181b5d69080e8e347f79
Andrei <chelya@tut.by>
2022-07-31 12:35:40 +0300

    feat: add code

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 module.exports = function check(str, bracketsConfig) {
+  let stack = [];
+  for(let i=0;i<str.length;i++){
+    let currentS = str[i];
+    let topEl = stack[stack.length - 1];
+    for (let j=0;j<bracketsConfig.length;j++){
+      let currentConfig = bracketsConfig[j];
+      if (currentConfig.includes(currentS)){
+        currentS == currentConfig[1]?
+        (topEl == currentConfig[0] ? stack.pop() : stack.push(currentS)):
+        stack.push(currentS);
+      }
+    }
+  }
+  return stack.length ===0;
   // your solution
 }
